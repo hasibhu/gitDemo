296d08507437c484e821cf7ed37c83bce98423cb
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-07-09 17:16:55 +0600

    again update

--- src/components/Mir.jsx
+++ src/components/Mir.jsx
@@ -1,10 +1,13 @@
+import Tanvir from "./Tanvir";
 
 
 const Mir = () => {
+    const x = 5;
     return (
         <div>
             <h1>Mir Sakhawat Hossen</h1>
             <span>this is new update</span>
+            <Tanvir/>
         </div>
     );
 };
