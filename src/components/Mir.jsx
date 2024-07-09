976ea3576c2deb120d1777ab0113d4cb8fe5fe88
tanvir244 <tr30365s25@gmail.com>
2024-07-09 17:05:44 +0600

    chainging second time mir bhai's component

--- src/components/Mir.jsx
+++ src/components/Mir.jsx
@@ -4,7 +4,8 @@ const Mir = () => {
     return (
         <div>
             <h1>Mir Sakhawat Hossen</h1>
-            <h2>I'm gonna chaning Mir bhai's component</h2>
+            <h2>I'm gonna chaning Mir bhai's component 1st (Update - 01)</h2>
+            <h2>I'm gonna chaning Mir bhai's component 1st (Update - 02)</h2>
         </div>
     );
 };
