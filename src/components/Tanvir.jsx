1d3ce2e76bf2f38de43c9984036ea0e89f811914
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-07-09 16:58:35 +0600

    git added form mir

--- src/components/Tanvir.jsx
+++ src/components/Tanvir.jsx
@@ -4,6 +4,9 @@ const Tanvir = () => {
     return (
         <div>
             <h1>Tanvir</h1>
+            <h1>Text added form Mir</h1>
+            <p>ok what happened right now</p>
+
         </div>
     );
 };
